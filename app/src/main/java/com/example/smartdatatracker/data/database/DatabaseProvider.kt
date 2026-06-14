80cf5f89bc36c2d8248cd5eb3b01bd1fcaa576a3
Zelalem Argaw <thelalemarg@gmail.com>
2026-06-14 11:49:00 +0300

    feat: add Room database singleton provider

---
+++ app/src/main/java/com/example/smartdatatracker/data/database/DatabaseProvider.kt
@@ -0,0 +1,27 @@
+package com.example.smartdatatracker.data.database
+
+import android.content.Context
+
+import androidx.room.Room
+
+object DatabaseProvider {
+
+    @Volatile
+    private var INSTANCE: AppDatabase? = null
+
+    fun getDatabase(
+        context: Context
+    ): AppDatabase {
+        return INSTANCE ?: synchronized(this){
+            val instance = Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "smart_data_tracker_db"
+            ).build()
+
+            INSTANCE = instance
+
+            instance
+        }
+    }
+}
