9c04877cecc1c04e620c736bee99d3eb27839624
Zelalem Argaw <thelalemarg@gmail.com>
2026-06-18 15:51:20 +0300

    feat: add DataStore settings and Room database architecture

--- app/src/main/java/com/example/smartdatatracker/ui/screens/DashboardScreen.kt
+++ app/src/main/java/com/example/smartdatatracker/ui/screens/DashboardScreen.kt
@@ -1,18 +1,30 @@
 package com.example.smartdatatracker.ui.screens
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.smartdatatracker.utils.mbToGb
+import com.example.smartdatatracker.viewmodel.DashboardViewModel
 
 @Composable
+fun DashboardScreen(
+    viewModel: DashboardViewModel = viewModel()
+) {
+    val usageList by viewModel.usageData.collectAsState()
 
-fun DashboardScreen(){
     Column(
-        modifier = Modifier.fillMaxSize().padding(20.dp),
-
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
@@ -31,6 +43,44 @@ fun DashboardScreen(){
                 Text("Monthly Plan: 30GB")
                 Text("Used: 0 GB")
                 Text("Remaining: 30GB")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        // Step 3: Add Test Button
+        Button(
+            onClick = {
+                viewModel.insertTestData()
+            }
+        ) {
+            Text("Insert Test Usage")
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        // Step 4: Display Data
+        usageList.forEach { usage ->
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp)
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text(
+                        text = "Phone: ${mbToGb(usage.phoneUsageMB)} GB"
+                    )
+
+                    Text(
+                        text = "Hotspot: ${mbToGb(usage.hotspotUsageMB)} GB"
+                    )
+
+                    Text(
+                        text = "Total: ${mbToGb(usage.totalUsageMB)} GB"
+                    )
+                }
             }
         }
     }
---
+++ app/src/main/java/com/example/smartdatatracker/viewmodel/DashboardViewModel.kt
@@ -0,0 +1,49 @@
+package com.example.smartdatatracker.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.room.Database
+import com.example.smartdatatracker.data.database.DatabaseProvider
+import com.example.smartdatatracker.data.database.entities.DailyUsage
+import com.example.smartdatatracker.data.repository.DailyUsageRepository
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class DashboardViewModel (
+    application: Application
+) : AndroidViewModel(application) {
+    private val repository: DailyUsageRepository
+
+    val usageData: StateFlow<List<DailyUsage>>
+
+    init {
+        val db = DatabaseProvider.getDatabase(application)
+
+        repository = DailyUsageRepository(
+            db.dailyUsageDao()
+        )
+
+        usageData = repository.getAllDailyUsages().stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000),
+            emptyList()
+        )
+    }
+
+    fun insertTestData(){
+
+        viewModelScope.launch {
+            repository.insertDailyUsage(
+                DailyUsage(
+                    date = System.currentTimeMillis(),
+                    phoneUsageMB = 5000,
+                    hotspotUsageMB = 2000,
+                    totalUsageMB = 7000
+                )
+            )
+        }
+    }
+}
